b83887b2b0f967cb7e0a144980e5ca75cc69a208
Saviyo George <saviyogeorge123@gmail.com>
2026-01-14 13:56:31 +0530

    first comm

--- models/doctor.js
+++ models/doctor.js
@@ -1,11 +1,27 @@
-const mongoose=require('mongoose');
-const Doctormodel=new mongoose.Schema({
-    user:{
-     type:mongoose.Schema.Types.ObjectId,
-     ref:"User",
-     required:true
-    },
-})
+// const mongoose=require('mongoose');
+// const Doctormodel=new mongoose.Schema({
+//     user:{
+//      type:mongoose.Schema.Types.ObjectId,
+//      ref:"User",
+//      required:true
+//     },
+// })
 
-const doctor=mongoose.model('doctor',Doctormodel);
-module.exports=doctor
+// const doctor=mongoose.model('doctor',Doctormodel);
+// module.exports=doctor
+
+
+const mongoose = require('mongoose');
+
+const doctorSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  }
+});
+
+const Doctor =
+  mongoose.models.Doctor || mongoose.model('Doctor', doctorSchema);
+
+module.exports = Doctor;
